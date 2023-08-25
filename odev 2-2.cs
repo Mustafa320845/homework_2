cbb73c075c1d6432f666bee296078c1d2d289a32
Mustafa320845 <142300211+Mustafa320845@users.noreply.github.com>
2023-08-25 19:35:50 +0300

    Add files via upload

---
+++ odev 2-2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mustafa_karacik
+{
+    internal class Program
+    {
+        class BankaHesabi
+        {
+            private double bakiye;
+
+            public double Bakiye
+            {
+                get { return bakiye; }
+                set { bakiye = value; }
+            }
+
+            public void ParaCek(double miktar)
+            {
+                if (miktar > bakiye)
+                {
+                    Console.WriteLine("Yetersiz bakiye.İşlem gerçekleştirilemedi.");
+                }
+                else
+                {
+                    bakiye -= miktar;
+                    Console.WriteLine($"{miktar} TL çekildi. Yeni bakiye: {bakiye} TL");
+                }
+            }
+        }
+
+        class Program1
+        {
+            static void Main(string[] args)
+            {
+                BankaHesabi hesap = new BankaHesabi();
+                hesap.Bakiye = 1000;
+
+                Console.Write("Çekilecek miktarı girin: ");
+                double miktar = Convert.ToDouble(Console.ReadLine());
+
+                hesap.ParaCek(miktar);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
